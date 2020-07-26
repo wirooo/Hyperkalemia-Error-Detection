43366475ab76e3b09c1b6d5fad82eb8799b68469
Raymond Lin <rlin264@gmail.com>
2020-07-26 16:24:00 -0400

    processing w/o pivot tables

--- Matching_Keys.py
+++ Matching_Keys.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import os
 import csv
-diagnosis = pd.read_csv(os.path.join(os.getcwd(), "diagnosis.csv"))
-icd9_key = pd.read_csv(os.path.join(os.getcwd(), "icd9_dx.csv"), encoding = 'latin1')
-newfile = "diagnosis_hypokalemia.csv"
+import processing
+
 def MatchingKeys(diagnosis_csv, icd9_dx_csv, newfilename):
     #extract the code and description from the icd9_key file
     key = icd9_key[['dx_code','long_desc']]
+    print(key)
     #Write a new dictionary linking codes to diseases
     dic = {}
-    for x,dx_code, long_desc in tqdm(key.itertuples()):
+    for x, dx_code, long_desc in key.itertuples():
         dic[dx_code] = long_desc
     #List of all the icd9 we see in diaganosis
+    diagnosis = processing.padding_icd9(diagnosis_csv, 'icd9code');
     m = diagnosis.icd9code.unique()
     l = []
     #Iterate through the list of codes and add their respective long descriptions
     for icd9 in m:
-        code = str(icd9).split(", ")[0]
+        # code = str(icd9).split(", ")[0]
+        code = icd9
         if(code in dic):
             l.append([code, dic[code]])
+        else:
+            print(code)
     #Write the CSV file
     with open(newfilename,'w', newline = '') as result_file:
         wr = csv.writer(result_file, delimiter = ',')
         for row in l:
             wr.writerow(row)
+
+diagnosis = pd.read_csv(os.path.join(os.getcwd(), "diagnosis1.csv"))
+icd9_key = pd.read_csv(os.path.join(os.getcwd(), "icd9_dx.csv"), encoding = 'latin1')
+newfile = "diagnosis_hypokalemia1.csv"
+MatchingKeys(diagnosis, icd9_key, newfile)
--- processing.py
+++ processing.py
@@ -29,11 +29,18 @@ def reformat_lab(df):
     del df['labresulttext']
     del df['labmeasurenameinterface']
     del df['labresultrevisedoffset']
-    res = df.pivot_table(index=['patientunitstayid', 'labresultoffset'], 
-                        columns='labname' )
-    res.reset_index(inplace=True)
-    # res.to_csv('test.csv')
-    return res
+    # del df['labresultoffset']
+    # res = df.pivot_table(index=['patientunitstayid', 'labresultoffset'], 
+                        # columns='labname' )
+    # res.reset_index(inplace=True)
+    # new_df = df.set_index(['labname','labresultoffset','patientunitstayid']).unstack('labname')
+    new_df = df.groupby(['labname', 'patientunitstayid'])['labresult'].max().unstack(['labname'],)
+    # print(list(new_df.columns))
+    # print(list(new_df.columns).remove("patientunitstayid"))
+    # new_df = new_df.groupby(["patientunitstayid"])[list(new_df.columns)].agg(lambda x: ';;'.join(x.astype(str)))
+    print(new_df)
+    new_df.to_csv('test.csv')
+    return new_df
 
 
 def match_icd9codes():
@@ -61,11 +68,11 @@ def padding_icd9(df, column):
     return df
 
 if __name__ == '__main__':
-    SERVER_NAME = "teamseven.ct4lx0aqwcg9.ca-central-1.rds.amazonaws.com"
-    DATABASE_NAME = "eicu_demo"
-    USERNAME = "admin"
-    PASSWORD = "CB5T1Nc2xvN6WPl6GBg3"
-    s = SqlClient(SERVER_NAME, DATABASE_NAME, USERNAME, PASSWORD)
+    # SERVER_NAME = "teamseven.ct4lx0aqwcg9.ca-central-1.rds.amazonaws.com"
+    # DATABASE_NAME = "eicu_demo"
+    # USERNAME = "admin"
+    # PASSWORD = "CB5T1Nc2xvN6WPl6GBg3"
+    # s = SqlClient(SERVER_NAME, DATABASE_NAME, USERNAME, PASSWORD)
     # df = s.select('diagnosis', select="patientunitstayid, icd9code, diagnosispriority")
     # print(df)
     # squashed = aggregate_common_rows(df,
@@ -75,7 +82,13 @@ if __name__ == '__main__':
     #                                  {'icd9code': "No Code"})
     # print(squashed)
 
-    df = pd.read_csv("diagnosis_hyperkalemia.csv")
-    formatted = padding_icd9(df, 'ICD9')
-    print(formatted)
-    s.upload_df(formatted, "diagnosis_hyperkalemia");
+    # df = pd.read_csv("diagnosis_hyperkalemia.csv")
+    # formatted = padding_icd9(df, 'ICD9')
+    # print(formatted)
+    # s.upload_df(formatted, "diagnosis_hyperkalemia");
+
+    df = pd.read_csv("lab.csv")
+    reformat_lab(df)
+    # df = df[df['labname'] == 'potassium']
+    print(df['patientunitstayid'].unique().shape)
+    # out = reformat_lab(df)
