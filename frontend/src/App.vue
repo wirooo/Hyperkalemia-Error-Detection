4abc15cd71c423d9fbb2b0950436f97230f4bffc
Will Lu <willh.lu@gmail.com>
2020-07-28 21:12:06 -0400

    add frotend demo

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Body></Body>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Body,
+  },
+};
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3 py-2">
+      <a class="navbar-brand" href="#">H.E.A.D.</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Overview</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Diagnosis</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
